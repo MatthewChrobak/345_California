a7d80401dbcb9919fd4f904bbe9a681bce21e373
theonlychunsta <lau_tsechun@hotmail.com>
2017-03-14 04:59:58 -0400

    PlayerCards
    PlayerCards methods

--- MainProject/CaliDemic/PlayerCard.h
+++ MainProject/CaliDemic/PlayerCard.h
@@ -1,6 +1,15 @@
 #pragma once
 #include "Paths.h"
 #include <string>
+#include <vector>
+#include <iostream>
+#include <random>
+
+using std::string;
+using std::vector;
+using std::cin;
+using std::cout;
+using std::endl;
 
 enum PlayerCardType
 {
@@ -9,11 +18,7 @@ enum PlayerCardType
 	Event_Card
 };
 
-class PlayerCard
-{
-public:
-	PlayerCard();
-	~PlayerCard();
+static class PlayerCard {
 
 	PlayerCardType getType();
 	virtual void playCard() = 0;
@@ -21,5 +26,42 @@ public:
 
 protected:
 	PlayerCardType _cardType;
-};
 
+public:
+	std::vector <string> playerCards;
+
+	int cardCounter = 48;   //This value will decrease as more cards are played.
+
+	vector <string> playerCardsDeck;
+
+	vector <string> epidemic
+	{
+		"Epidemic",
+		"Epidemic",
+		"Epidemic",
+		"Epidemic"
+	};
+
+public:
+	PlayerCard();
+	~PlayerCard();
+
+public:
+	
+	string getPlayerCards();
+	string getSingleCard(int cardPosition);
+
+	int getCardCounter();               //Counter for number of cards left in PlayerCards
+	int getVectorSize();                //Retrieves the size of a vector
+
+	void removeSingleCard(int cardPosition);             //Removes a single card from a vector at a specific position
+	void setPlayerCards(int numberOfCards);              //Sets a number of cards to a player
+	void clearVector();                                  //Clears a vector from all its values
+	void getEventDescription();                          //Retrieves the description of an event card
+	void getEpidemicDescription();                       //Retrieves the description of an epidemic card
+	void buildStationPC();                               //Discards enough cards to build a research station
+	void useEpidemic();                                  //Discards Epidemic cards if player picks one
+	void setPlayerCardDeck();
+
+	 //void listCities(); //To test the list of cities remaining in PlayerCards.
+};
