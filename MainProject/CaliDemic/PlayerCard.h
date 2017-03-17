4de1cd54c0fbb3177b5457e05838c8db28ceb0d9
theonlychunsta <lau_tsechun@hotmail.com>
2017-03-17 07:23:33 -0400

    PlayerCard clean up

--- MainProject/CaliDemic/PlayerCard.h
+++ MainProject/CaliDemic/PlayerCard.h
@@ -3,13 +3,6 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <random>
-
-using std::string;
-using std::vector;
-using std::cin;
-using std::cout;
-using std::endl;
 
 enum PlayerCardType
 {
@@ -17,7 +10,6 @@ enum PlayerCardType
 	Epidemic_Card,
 	Event_Card
 };
-
 
 
 class PlayerCard {
@@ -31,28 +23,14 @@ protected:
 	PlayerCardType _cardType;
 
 public:
-	vector <string> playerCards;
-	vector <string> playerCardsDeck;
+	std::vector <std::string> playerCardsDeck;
 
 public:
 	PlayerCard();
 	~PlayerCard();
 
 public:
-	
-	string getPlayerCards();
-	string getSingleCard(int cardPosition);
 
-	void playerCardGameOver();          //Game over if playerCardsDeck = 0
-	int getVectorSize();                //Retrieves the size of a vector
-
-	void removeSingleCard(int cardPosition);             //Removes a single card from a vector at a specific position
-	void setPlayerCards(int numberOfCards);              //Sets a number of cards to a player
-	void clearVector();                                  //Clears a vector from all its values
-	void getEventDescription();                          //Retrieves the description of an event card
-	void getEpidemicDescription();                       //Retrieves the description of an epidemic card
-	void buildStationPC();                               //Discards enough cards to build a research station
-	void useEpidemic();                                  //Discards Epidemic cards if player picks one
-	void setPlayerCardDeck();
+	void useEpidemic();
 
 };
