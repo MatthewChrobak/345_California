7ccbe53b370226c5d2786c4ec9d81ee3a75edca7
pixelsoccupied <nahian@me.com>
2017-04-04 23:13:44 -0400

    Add files via upload

--- Map implementation using BOOST/main.cpp
+++ Map implementation using BOOST/main.cpp
@@ -5,176 +5,84 @@
 using namespace boost;
 using namespace std;
 
-#include "Vertex.h"
-#include "Edge.h"
-#include "Board.h"
+#include "Player.h"
 #include <boost/graph/graphviz.hpp>
 #include <boost/graph/erdos_renyi_generator.hpp>
 #include <boost/random.hpp>
 #include <boost/graph/connected_components.hpp>
+#include "PandemicEngine.h"
+#include "PlayerRoleCard.h"
 
 typedef MyGraph::vertex_descriptor VertexID;
 typedef MyGraph::edge_descriptor EdgeID;
-//typedef boost::erdos_renyi_iterator<boost::minstd_rand, MyGraph> ERGen;
 
+using namespace boost;
 
 
 int main() {
 
 
-    Board board;
+    PandemicEngine* pe = new PandemicEngine();
+
+    cout << "\nTotal number vertices ->" << num_vertices(pe->getBoard()->getGraph()) << endl;
+    cout << "\nTotal number edges ->" << num_edges(pe->getBoard()->getGraph()) << endl;
 
 
-    //int x = vID2;
+    PlayerCard pc1("Montreal");
+    PlayerCard pc2("Atlanta");
+    PlayerCard pc3("Chicago");
+    PlayerCard pc4("Essen");
+    PlayerCard pc5("London");
+    PlayerCard pc6("Madrid");
 
-    cout << "\nAdding multiple cities - " << endl;
-    vector<string> blueCities
-            {
-                    "Atlanta",      // blueCities[0]
-                    "Chicago",
-                    "Essen",
-                    "London",
-                    "Madrid",       // blueCities[4]
-                    "Milan",
-                    "New York",
-                    "Paris",
-                    "San Francisco",// blueCities [8]
-                    "St Petersburg",
-                    "Montreal",
-                    "Washington",
-                    "Bogota",       // yellowCities[0]
-                    "Buenos Aires",
-                    "Johannesburg",
-                    "Khartoum",
-                    "Kinshasa",     // yellowCities[4]
-                    "Lagos",
-                    "Lima",
-                    "Los Angeles",
-                    "Mexico",  // yellowCities[8]
-                    "Miami",
-                    "Santiago",
-                    "Sao Paulo",
-                    "Algiers",      // blackCities[0]
-                    "Baghdad",
-                    "Cairo",
-                    "Chennai",
-                    "Delhi",        // blackCities[4]
-                    "Istanbul",
-                    "Karachi",
-                    "Kolkata",
-                    "Moscow",       // blackCities[8]
-                    "Mumbai",
-                    "Riyadh",
-                    "Tehran",
-                    "Bangkok",      // redCities[0]
-                    "Beijing",
-                    "Ho Chi Minh",
-                    "Hong Kong",
-                    "Jakarta",      // redCities[4]
-                    "Manila",
-                    "Osaka",
-                    "Seoul",
-                    "Shanghai",     // redCities[8]
-                    "Sydney",
-                    "Taipei",
-                    "Tokyo"
-            };
+    vector <PlayerCard*> pcVector = {&pc1,&pc2,&pc3,&pc4,&pc5,&pc6};
 
-    for (int i = 0; i < blueCities.size(); i++) {
+    Pawn* pawn1 = new Pawn(pe->getBoard());
 
-        //cout<< "ADDING CITY ->" << blueCities[i] << " to graph" << endl;
-        board.addCity(blueCities[i]);
+    DirectFlightAction directFlightAction;
+    DiscoverCure discoverCure;
+    PlayerAction* pcArray [] = {&directFlightAction, &discoverCure};
+
+    Player* p = new Player;
 
 
-    }
+    p->setCurrentLocation(pawn1);
+    p->setPlayerCard(pcVector);
+    p->setPlayerAction(pcArray[0]);
+
+    cout << p->getCurrentLocation()->getCurrentCity()->getCityName() << endl;
+
+    p->usePlayerAction();
+
+    cout << p->getCurrentLocation()->getCurrentCity()->getCityName() << endl;
+
+    //Decorater class
+    PlayerRoleCard* playerRoleCard = new Scientist(pcArray[1]);
+    p->setPlayerAction(playerRoleCard);
+    p->usePlayerAction();
 
 
 
 
 
 
-
-    cout << "\nTotal number vertices ->" << num_vertices(board.getGraph()) << endl;
-    cout << "\nTotal number edges ->" << num_edges(board.getGraph()) << endl;
-
-
-
-
-    board.printMapContent();
-    board.searchAdjecentCities("Montreal");
-    board.cityExists("Atlanta");
-
-    board.addEdges();
-
-    board.searchAdjecentCities("Montreal");
-    board.cityExists("Atlanta");
-
-
-    //write_graphviz(cout, board.getGraph());
-    cout << "\nTotal number vertices ->" << num_vertices(board.getGraph()) << endl;
-    cout << "\nTotal number edges ->" << num_edges(board.getGraph()) << endl;
-
-
-    std::vector<int> component(num_vertices(board.getGraph()));
-    int num = connected_components(board.getGraph(), &component[0]);
-
-    std::vector<int>::size_type i;
-    cout << "Total number of components: " << num << endl;
-    /*for (i = 0; i != component.size(); ++i){
-        cout << "Vertex " << i <<" is in component " << component[i] << endl;
-    cout << endl;
-    }*/
-    //board.infectCity("Montreal");
-
-    string response;
-    cout << "Find the connected cities" << endl;
-    getline (std::cin,response);
-
-
-
-    int counter =0;
-    while (counter<10){
-
-
-        cout << "Try another one!" << endl;
-        getline (std::cin,response);
-        board.searchAdjecentCities(response);
-        counter++;
-
-    }
-
-
-
+    delete playerRoleCard;
+    delete p;
+    delete pe;
     cout << "\nProgram ended" << endl;
+
+
+
+
     return 0;
-
 }
-   // std :: deque <InfectionCard> ic;
-   // InfectionCard icc;
-   // ic.push_back(icc);
-
-//    icc.useInfectionCard(board);
 
 
 
-  //  cout << "\nTotal number vertices ->"<<  num_vertices(board.getGraph()) << endl;
-
-/*
-    boost::minstd_rand gen;
-    // Create graph with 100 nodes and edges with probability 0.05
-
-    MyGraph m(ERGen(gen, 100, 0.005), ERGen(), 100);
-    cout << num_vertices(m) << endl;
-    cout << num_edges(m) << endl;
-*/
-
-    //write_graphviz(cout, board.getGraph());
-   // print_dependencies(std::cout, board.getGraph(), get(vertex_name, board.getGraph()), get(edge_name, board.getGraph()) );
 
 
 
-   // return 0;
-    //    board.infectCity("Montreal");
+
 
 
 
