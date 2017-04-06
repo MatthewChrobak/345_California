b5fb474f9856c1924b352c7a0b6447141e3ea792
Matthew Chrobak <matthewchrobak@hotmail.com>
2017-04-06 18:54:51 -0400

    Boost Filesystem
    - Added support for Boost in the filesystem class

--- Pandemic/src/Paths.h
+++ Pandemic/src/Paths.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#define CARDS_PATH "cards/"
-#define GRAPHICS_PATH "graphics/"
-#define FONTS_PATH "fonts/"
+#define CARDS_PATH "\\cards\\"
+#define GRAPHICS_PATH "\\graphics\\"
+#define FONTS_PATH "\\fonts\\"
 
-#define GAME_SAVES_FOLDER "saves/"
+#define GAME_SAVES_FOLDER "/saves/"
 #define NODES_DATA_FILE "nodes.dat"
 #define PLAYER_DATA_FILE "players.dat"
 #define BOARD_DATA_FILE "board.dat"
