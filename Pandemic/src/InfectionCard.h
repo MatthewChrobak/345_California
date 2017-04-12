e8e660b817a74ae84e7d3284d83e5a0a6952066e
darkheros12 <g.poseidon@hotmail.com>
2017-04-11 23:39:13 -0400

    City->InfectionCard
    Move function from City class to InfectionCard Class

--- Pandemic/src/InfectionCard.h
+++ Pandemic/src/InfectionCard.h
@@ -1,14 +1,17 @@
 #pragma once
 #include "City.h"
 #include "InfectionColors.h"
+#include "GuiManager.h"
 
 class InfectionCard
 {
 public:
 	InfectionCard();
 	~InfectionCard();
-	static void infectCityCube(int city);
 
+	static void infectCityCube(int city);
+	void infectCity(int city, int index);
+	void infectCityOutBreak(int city);
 
 };
 
